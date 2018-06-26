ad36a57ea60e92490d0f15963634ce49512ee501
Madisen Arurang <madireyn@uw.edu>
2018-06-26 15:41:55 -0700

    pushing files

---
+++ mvc/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld.Welcome/
+
+        public ActionResult Welcome(string name, int NumTimes = 1)
+        {
+            ViewBag.message = "Hello " + name;
+            // ViewBag.mensaje = "Hola, como estas " + name + "?";
+            ViewBag.NumTimes = NumTimes;
+
+            return View();
+        }
+    }
+}
